957f2ef8485be1d51f081784d571b9523c2f1cd3
sskates1 <simonirv2@gmail.com>
2014-06-05 14:32:55 -0400

    added database connections and initialize recipe method

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,5 +1,37 @@
+require 'pg'
+
+def db_connection
+  begin
+    connection = PG.connect(dbname: 'recipes')
+
+    yield(connection)
+
+  ensure
+    connection.close
+  end
+end
+
+
 class Recipe
+  attr_reader :id, :name
+
+  def initialize(id)
+    query = "SELECT * FROM recipes WHERE id = $1"
+    recipe = db_connection do |conn|
+      conn.exec_params(query,[id])
+    end
+    @id = id
+    @name = recipe.to_a[0]["name"]
+  end
+
 
   def self.all
+    query = "SELECT * FROM recipes"
+    recipes = db_connection do |conn|
+      conn.exec(query)
+    end
+    return recipes.to_a
   end
+
+
 end
